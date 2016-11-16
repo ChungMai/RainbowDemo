57811014a80c28e84c0abe5f726cd1e12d7836b7
ChungMai <macmini@MacMinis-Mac-mini.local>
2016-11-16 18:04:34 +0700

    Learn about IBDesignable_IBInspectable
    https://www.appcoda.com/ibdesignable-ibinspectable-tutorial/

---
+++ RainbowDemo/Rainbow.swift
@@ -0,0 +1,24 @@
+//
+//  Rainbow.swift
+//  RainbowDemo
+//
+//  Created by MacMini on 11/16/16.
+//  Copyright © 2016 Home. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class Rainbow: UIView {
+    @IBInspectable var firstColor: UIColor = UIColor(red: (37.0/255.0), green: (252.0/255), blue: (244.0/255.0), alpha: 1.0)
+    @IBInspectable var secondColor: UIColor = UIColor(red: (171.0/255.0), green: (250.0/255), blue: (81.0/255.0), alpha: 1.0)
+    @IBInspectable var thirdColor: UIColor = UIColor(red: (238.0/255.0), green: (32.0/255), blue: (53.0/255.0), alpha: 1.0)
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+}
