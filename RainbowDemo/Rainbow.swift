8b4634d1a3df2e8de6518256286ff316a9a36028
ChungMai <macmini@MacMinis-Mac-mini.local>
2016-11-17 17:45:49 +0700

    Done
    Done

--- RainbowDemo/Rainbow.swift
+++ RainbowDemo/Rainbow.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreGraphics
 
 @IBDesignable
 class Rainbow: UIView {
@@ -20,5 +21,47 @@ class Rainbow: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+    }
+    
+    func addOval(lineWidth: CGFloat, path: CGPath, strokeStart: CGFloat, strokeEnd: CGFloat, strokeColor: UIColor, fillColor: UIColor, shadowRadius: CGFloat, shadowOpacity: Float, shadowOffsset: CGSize) {
+        
+        let arc = CAShapeLayer()
+        arc.lineWidth = lineWidth
+        arc.path = path
+        arc.strokeStart = strokeStart
+        arc.strokeEnd = strokeEnd
+        arc.strokeColor = strokeColor.cgColor
+        arc.fillColor = fillColor.cgColor
+        arc.shadowColor = UIColor.black.cgColor
+        arc.shadowRadius = shadowRadius
+        arc.shadowOpacity = shadowOpacity
+        arc.shadowOffset = shadowOffsset
+        layer.addSublayer(arc)
+    }
+    
+    override func draw(_ rect: CGRect) {
+        // Add ARCs
+        self.addCirle(arcRadius: 80, capRadius: 20, color: self.firstColor)
+        self.addCirle(arcRadius:150, capRadius: 20, color: self.secondColor)
+        self.addCirle(arcRadius:215, capRadius: 20, color: self.thirdColor)
+    }
+    
+    func addCirle(arcRadius: CGFloat, capRadius: CGFloat, color: UIColor) {
+        let X = self.bounds.midX
+        let Y = self.bounds.midY
+        
+//        // Bottom Oval
+        let pathBottom = UIBezierPath(ovalIn: CGRect(origin: CGPoint(x:(X - (arcRadius/2)), y:(Y - (arcRadius/2))), size: CGSize(width: arcRadius, height: arcRadius))).cgPath
+        self.addOval(lineWidth: 20.0, path: pathBottom, strokeStart: 0, strokeEnd: 0.5, strokeColor: color, fillColor: UIColor.clear, shadowRadius: 0, shadowOpacity: 0, shadowOffsset: CGSize.zero)
+//
+//        // Middle Cap
+        let pathMiddle = UIBezierPath(ovalIn: CGRect(origin: CGPoint(x:(X - (capRadius/2)) - (arcRadius/2), y:(Y - (capRadius/2))), size: CGSize(width:capRadius, height: capRadius))).cgPath
+        self.addOval(lineWidth: 0.0, path: pathMiddle, strokeStart: 0, strokeEnd: 1.0, strokeColor: color, fillColor: color, shadowRadius: 5.0, shadowOpacity: 0.5, shadowOffsset: CGSize.zero)
+        
+        // Top Oval
+        let pathTop = UIBezierPath(ovalIn: CGRect(origin: CGPoint(x:(X - (arcRadius/2)), y:(Y - (arcRadius/2))),size: CGSize(width: arcRadius, height: arcRadius))).cgPath
+        
+        self.addOval(lineWidth: 20.0, path: pathTop, strokeStart: 0.5, strokeEnd: 1.0, strokeColor: color, fillColor: UIColor.clear, shadowRadius: 0, shadowOpacity: 0, shadowOffsset: CGSize.zero)
+        
     }
 }
